8595de24869ed706850703ac63cf3586afe7f705
samiaaziz1457 <samiaaziz502@gmail.com>
2025-04-12 00:17:22 +0500

    4/12/25

---
+++ lib/views/auth/CrouselSlider.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class Crouselslider extends StatelessWidget {
+  const Crouselslider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       
+    );
+  }
+}
--- lib/views/auth/otp.dart
+++ lib/views/auth/otp.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+
 import 'package:app/views/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
@@ -49,7 +50,7 @@ class OtpScreen extends StatelessWidget {
         onPressed: () {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) =>Homescreen()), // Replace your screen
+        MaterialPageRoute(builder: (context) =>HomeScreen()), // Replace your screen
       );
         },
         style: ElevatedButton.styleFrom(
